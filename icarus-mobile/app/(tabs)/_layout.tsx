d077413be34171ede9218b56c5191c467aaae8af
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-06-14 12:09:32 -0700

    Added page animation for clean load

--- icarus-mobile/app/(tabs)/_layout.tsx
+++ icarus-mobile/app/(tabs)/_layout.tsx
@@ -3,10 +3,38 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="login" options={{ headerShown: false, animation: "none", gestureEnabled: false }} />
-      <Stack.Screen name="signup" options={{ headerShown: false, animation: "none", gestureEnabled: false }} />
-      <Stack.Screen name="home" options={{ headerShown: false, animation: "none", gestureEnabled: false }} />
-      <Stack.Screen name="previousentries" options={{ headerShown: false, animation: "none", gestureEnabled: false }} />
+      <Stack.Screen
+        name="login"
+        options={{
+          headerShown: false,
+          animation: "ios",
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name="signup"
+        options={{
+          headerShown: false,
+          animation: "ios",
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          animation: "ios",
+          gestureEnabled: false,
+        }}
+      />
+      <Stack.Screen
+        name="previousentries"
+        options={{
+          headerShown: false,
+          animation: "ios",
+          gestureEnabled: false,
+        }}
+      />
     </Stack>
   );
 }
--- icarus-mobile/app/_layout.tsx
+++ icarus-mobile/app/_layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout() {
           headerTitle: "",
           headerTransparent: true,
           headerShown: false,
-          animation: "none",
+          animation: "ios",
           gestureEnabled: false,
         }}
       />
